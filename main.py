c5d29ae34725eb17227527693311696aaed05709
TulKasTer <yuni.bastidas@gmail.com>
2025-03-21 01:39:31 -0700

    Correciones de Protanopia

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Se define la funcion para hacer el filtro de protanopia
 def apply_protanopia_filter(image):
     # Matriz de transformación para Protanopía
     protanopia_kernel = np.array([
@@ -11,6 +12,8 @@ def apply_protanopia_filter(image):
     ])
     
     # Aplicar la transformación
+
+    # Se realizar la multiplicacion Matricial
     protanopia_image = image @ protanopia_kernel.T 
     protanopia_image = np.clip(protanopia_image, 0, 255).astype(np.uint8)
     
@@ -20,6 +23,7 @@ def apply_protanopia_filter(image):
 image_path = 'images/image6.jpg.webp'
 image = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
+# En caso de que no exista, dara un error.
 if image is None:
     print("Error: No se pudo cargar la imagen.")
     exit()
