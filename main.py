2eee4daf57600240635d06ea2a4091ed1eb8d648
Nick-2455 <nicolaspacheco2455@gmail.com>
2025-03-21 10:47:26 -0700

    Deutaronopia y Tritanopia

--- main.py
+++ main.py
@@ -2,40 +2,45 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def apply_colorblind_filter(image):
-    colorblind_matrix = np.array([[0.43, 0.72, -0.15],
-                                  [0.34, 0.57, 0.09],
-                                  [-0.02, 0.03, 1.00]])
+def apply_deuteranopia_filter(image):
+    deuteranopia_matrix = np.array([[0.43, 0.72, -0.15],
+                                    [0.34, 0.57, 0.09],
+                                    [-0.02, 0.03, 1.00]])
     
-    corrected_image = cv2.transform(image, colorblind_matrix)
+    corrected_image = cv2.transform(image, deuteranopia_matrix)
     corrected_image = np.clip(corrected_image, 0, 255).astype(np.uint8)
     return corrected_image
 
 def apply_tritanopia_filter(image):
     tritanopia_matrix = np.array([[1.0, 0.0, 0.0],
-                              [0.0, 1.0, 0.0],
-                              [-0.395, 0.801, 0.0]])  
+                                  [0.0, 1.0, 0.0],
+                                  [-0.395, 0.801, 0.0]])  
 
     corrected_image = cv2.transform(image, tritanopia_matrix)
     corrected_image = np.clip(corrected_image, 0, 255).astype(np.uint8)
     return corrected_image
 
+# Cargar imagen
 image = cv2.imread("images/paisaje.jpg", cv2.IMREAD_COLOR)
 
 if image is None:
     print("Error: No se pudo cargar la imagen.")
     exit()
 
-colorblind_result = apply_colorblind_filter(image)
-
-#Tritanopia
+# Aplicar filtros
+deuteranopia_result = apply_deuteranopia_filter(image)
 tritanopia_result = apply_tritanopia_filter(image)
 
-fig, axes = plt.subplots(1, 2, figsize=(10, 4))
+# Mostrar imágenes
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
-# Cambiado para recibir el resultado tritanopia, sera resolvido en el merge.
-images = [cv2.cvtColor(image, cv2.COLOR_BGR2RGB), cv2.cvtColor(tritanopia_result, cv2.COLOR_BGR2RGB)]
-titles = ["Imagen Original", "Filtro Tritanopia"]
+images = [
+    cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
+    cv2.cvtColor(deuteranopia_result, cv2.COLOR_BGR2RGB),
+    cv2.cvtColor(tritanopia_result, cv2.COLOR_BGR2RGB)
+]
+
+titles = ["Imagen Original", "Filtro Deuteranopia", "Filtro Tritanopia"]
 
 for ax, img, title in zip(axes, images, titles):
     ax.imshow(img)
@@ -44,4 +49,3 @@ for ax, img, title in zip(axes, images, titles):
 
 plt.tight_layout()
 plt.show()
-exit()
