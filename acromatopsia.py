370c661d0bfb92c91fac11c680efe828b13e79b2
rubencastro69 <a01255344@tec.mx>
2025-03-21 01:43:35 -0700

    Versión final con comentarios

--- acromatopsia.py
+++ acromatopsia.py
@@ -2,32 +2,39 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def apply_acromatopsia_filter(image):
-    # Convertir la imagen a escala de grises
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#matríz para que el filtro funcione
+def apply_acromatopsia_filter(imagen):
+    #en mi caso, necesito convertir a escala de grises, porque el daltonismo que manejo, es el de la gente que literalmente no ve colores
+    kernel = np.array([[0.299, 0.587, 0.114],
+                        [0.299, 0.587, 0.114],
+                        [0.299, 0.587, 0.114]])
     
-    # Convertir la imagen en un formato BGR para mantener compatibilidad con la visualización
-    corrected_image = cv2.cvtColor(grayscale_image, cv2.COLOR_GRAY2BGR)
-    
-    return corrected_image
+    imagen_filtro = cv2.transform(imagen, kernel) #transforma usando la matríz
+    imagen_filtro = np.clip(imagen_filtro, 0, 255).astype(np.uint8) #aseguro que el rango esté de 0 a 255 y convierte a uint8
+    return imagen_filtro
 
-image = cv2.imread('imagenes/fresas.jpeg', cv2.IMREAD_COLOR)
+#declaro imagen, para usar una imagen diferente, se tiene que cambiar a "carpeta/imagen.tipo"
+imagen = cv2.imread("imagenes/fresas.jpeg", cv2.IMREAD_COLOR)
 
-if image is None:
+#mensaje de error por si no se detecta imagen
+if imagen is None:
     print("Error: No se pudo cargar la imagen.")
     exit()
 
-colorblind_result = apply_acromatopsia_filter(image)
+#aplica el filtro de escala de grises
+colorblind_result = apply_acromatopsia_filter(imagen)
 
-fig, axes = plt.subplots(1, 2, figsize=(10, 4))
-images = [cv2.cvtColor(image, cv2.COLOR_BGR2RGB), cv2.cvtColor(colorblind_result, cv2.COLOR_BGR2RGB)]
-titles = ["Imagen Original", "Filtro Acromatopsia"]
 
-for ax, img, title in zip(axes, images, titles):
+fig, axes = plt.subplots(1, 2, figsize=(10, 4)) #crea la figura con las 2 imágenes para su comparación
+imagenes = [cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB), cv2.cvtColor(colorblind_result, cv2.COLOR_BGR2RGB)] #convierte BGR a RGB
+titles = ["imagen Original", "Filtro Acromatopsia"] #títulos de las imágenes
+
+#muestra las imagenes en el mismo gráfico
+for ax, img, title in zip(axes, imagenes, titles):
     ax.imshow(img)
     ax.set_title(title)
-    ax.axis("off")
+    ax.axis("off") #oculta los ejes
 
-plt.tight_layout()
-plt.show()
-exit()
+plt.tight_layout() #ajusta el diseño
+plt.show() #muestra la figura con las 2 imágenes
+exit() #finaliza la ejecución del código
